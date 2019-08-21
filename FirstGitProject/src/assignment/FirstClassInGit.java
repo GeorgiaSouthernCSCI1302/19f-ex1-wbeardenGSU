a721d19f8d337a2bc86c426dd2e23805626e163b
wb03537 <wb03537@CEIT2553214X009.ad.georgiasouthern.edu>
2019-08-21 16:55:56 -0400

    Added an input function for my last name

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -1,12 +1,17 @@
 package assignment;
-
+import java.util.Scanner;
 public class FirstClassInGit {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 		System.out.println("Hello World!");
 		
 		//add a statement to display your name in the console
 		System.out.println("My name is William!");
+		System.out.println("My name last name is: ");
+		String lastName = input.next();
+		
+		System.out.println("My name is William " + lastName + "!");
 		//Then commit your changes
 
 	}
